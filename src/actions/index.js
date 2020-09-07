6ee7a87541acf5d938a17f93cfc59f20200bb2d7
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-07 13:14:34 +0900

    added redux-form and create post action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-const FETCH_CARS = 'FETCH_CARS';
+export const FETCH_CARS = 'FETCH_CARS';
+export const POST_CAR = 'POST_CAR';
 const URL = 'https://wagon-garage-api.herokuapp.com/';
 const API_KEY = 'garage4649';
 
@@ -9,4 +10,17 @@ export function fetchCars() {
     type: FETCH_CARS,
     payload: promise
   };
+}
+
+export function createCar(body) {
+  const promise = fetchCars(`${URL}${API_KEY}/cars`, {
+    method: 'POST',
+    headers: { Content-Type: 'application/json'},
+    body: JSON.stringify(body)
+  }).then(r => r.json());
+
+  return {
+    type: POST_CAR,
+    payload: promise
+  }
 }
